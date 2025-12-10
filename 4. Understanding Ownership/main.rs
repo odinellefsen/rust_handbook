2015db99c06145bd8569d68380a239bcbdd03fa2
odinellefsen <odin@ellefsen.fo>
2025-12-10 21:29:24 +0000

    Refactor string length calculation to use a reference, simplifying the main function and updating the calculate_length function to return only the length.

--- 4. Understanding Ownership/main.rs
+++ 4. Understanding Ownership/main.rs
@@ -1,13 +1,11 @@
 fn main() {
     let s1 = String::from("hello");
 
-    let (s2, len) = calculate_length(s1);
+    let len = calculate_length(&s1);
 
-    println!("The length of '{s2}' is {len}.");
+    println!("The length of '{s1}' is {len}.");
 }
 
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len(); // len() returns the length of a String
-
-    (s, length)
+fn calculate_length(s: &String) -> usize {
+    return s.len();
 }
